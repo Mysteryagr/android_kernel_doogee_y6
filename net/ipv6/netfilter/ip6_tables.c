730d114749174a48950f5f1dbc7135a1478d6e7b
Ahmed Rahmy <ahmedrahmy842@gmail.com>
2017-12-11 00:47:27 +0200

    Fix a mistake
    Removed by mistake while fixing conflict in tag v3.18.37 (Upstreaming)

--- net/ipv6/netfilter/ip6_tables.c
+++ net/ipv6/netfilter/ip6_tables.c
@@ -758,7 +758,7 @@ check_entry_size_and_hooks(struct ip6t_entry *e,
 			if (!check_underflow(e)) {
 				pr_debug("Underflows must be unconditional and "
 					 "use the STANDARD target with "
-					 "ACCEPT/DROP\n")
+					 "ACCEPT/DROP\n");
 				return -EINVAL;
 			}
 			newinfo->underflow[h] = underflows[h];
